d9c7a4c17cd5793e6c43b5ac0471879aff4e60dd
How <mr.how-yu@outlook.com>
2026-07-08 19:56:59 +0800

    Add source: src/components/Piece.tsx

---
+++ src/components/Piece.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+
+// Import piece images
+import bishopBlack from '../assets/bishop-black.png';
+import bishopWhite from '../assets/bishop-white.png';
+import kingBlack from '../assets/king-black.png';
+import kingWhite from '../assets/king-white.png';
+import knightBlack from '../assets/knight-black.png';
+import knightWhite from '../assets/knight-white.png';
+import pawnBlack from '../assets/pawn-black.png';
+import pawnWhite from '../assets/pawn-white.png';
+import queenBlack from '../assets/queen-black.png';
+import queenWhite from '../assets/queen-white.png';
+import rookBlack from '../assets/rook-black.png';
+import rookWhite from '../assets/rook-white.png';
+
+type PieceType = 'pawn' | 'rook' | 'knight' | 'bishop' | 'queen' | 'king';
+type PieceColor = 'white' | 'black';
+
+interface PieceProps {
+  type: PieceType;
+  color: PieceColor;
+  small?: boolean;
+}
+
+const pieceImages: Record<PieceType, Record<PieceColor, string>> = {
+  king: { white: kingWhite, black: kingBlack },
+  queen: { white: queenWhite, black: queenBlack },
+  rook: { white: rookWhite, black: rookBlack },
+  bishop: { white: bishopWhite, black: bishopBlack },
+  knight: { white: knightWhite, black: knightBlack },
+  pawn: { white: pawnWhite, black: pawnBlack },
+};
+
+export const Piece: FC<PieceProps> = ({ type, color, small }) => {
+  return (
+    <div
+      className={`w-full h-full cursor-grab active:cursor-grabbing flex items-center justify-center ${
+        small ? 'scale-75 cursor-default' : ''
+      }`}
+    >
+      <img
+        src={pieceImages[type][color]}
+        alt={`${color} ${type}`}
+        className='w-[80%] h-[80%] object-contain'
+      />
+    </div>
+  );
+};

e3b14d5cde6dd4aea0be16dadae026cce436b44c
How <mr.how-yu@outlook.com>
2026-07-08 19:56:35 +0800

    Add source: src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Board } from './components/Board';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='min-h-screen w-full bg-gray-900 flex flex-col'>
+      <Header />
+      <div className='flex-1 flex flex-col lg:flex-row items-center md:justify-center p-4 gap-4 max-sm:mt-4'>
+        <div className='bg-gray-800 rounded-lg shadow-lg p-4'>
+          <Board />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
